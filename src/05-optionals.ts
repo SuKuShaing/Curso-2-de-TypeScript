0d6037da1fa93224ca481bab421303fc1ddb7146
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-06-19 14:21:01 -0400

    se añaden comentarios al opcional

--- src/05-optionals.ts
+++ src/05-optionals.ts
@@ -5,7 +5,7 @@ export const createProduct = (
 ) => {
 	return {
 		id,
-		stock: stock ?? 10,
+		stock: stock ?? 10,  // es mejor usar el operador ?? que el ||, puesto que el || toma el valor 0,"" y false como false, en cambio el ?? toma el valor undefined y null como false y pasará a tomar el siguiente valor
 		isNew: isNew ?? true,
 	};
 };
@@ -22,15 +22,17 @@ console.log(p3);
 const p4 = createProduct(4, false, 0);
 console.log(p4);
 
+
+// -----------
 const createProduct2 = (
 	id: string | number,
-	isNew: boolean = true,
+	isNew: boolean = true, // Los valores por defecto también se pueden colocar aquí
 	stock: number = 10
 ) => ({ id, stock, isNew });
 
 const pp1 = createProduct2(1, true, 12);
 console.log(pp1);
-const pp2 = createProduct2(1);
+const pp2 = createProduct2(2);
 console.log(pp2);
-const pp3 = createProduct2(1, false, 0);
+const pp3 = createProduct2(3, false, 0);
 console.log(pp3);
