640f8dceaa332366f0c3089e57aad7287e8f0f4e
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-06-23 15:37:17 -0400

    aplicación de un readonly

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -3,5 +3,8 @@ import { Product } from "./product.model";
 export const products: Product[] = [];
 
 export const addProduct = (data: Product) => {
+    // data.id = "1"; // lanza error por ser readonly
+    // data.createdAt = new Date(); // lanza error por ser readonly
+    // data.updatedAt = new Date();
     products.push(data);
 }
