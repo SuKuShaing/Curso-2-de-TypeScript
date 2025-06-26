747b0454757af8bc86b5bf8a7da7fc36f6c241b0
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-06-26 11:35:11 -0400

    creación de productos con faker-js

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,16 +1,27 @@
-import { addProduct } from "./products/product.service";
+import { addProduct, products } from './products/product.service';
+import { faker } from '@faker-js/faker';
 
-addProduct({
-    id: "1",
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    title: "Product 1",
-    stock: 10,
-    size: "S",
-    category: {
-        id: "1",
-        name: "Category 1",
-        createdAt: new Date(),
-        updatedAt: new Date()
-    },
-});
+for (let index = 0; index < 50; index++) {
+	addProduct({
+		id: faker.string.uuid(),
+		title: faker.commerce.productName(),
+		createdAt: faker.date.recent(),
+		updatedAt: faker.date.recent(),
+		stock: faker.number.int({ min: 10, max: 100 }),
+        size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
+		category: {
+			id: faker.string.uuid(),
+			name: faker.commerce.department(),
+			createdAt: faker.date.recent(),
+			updatedAt: faker.date.recent(),
+		},
+		image: faker.image.url(),
+		description: faker.commerce.productDescription(),
+		color: faker.color.rgb(),
+		isNew: faker.datatype.boolean(),
+		tags: faker.helpers.arrayElements(['tag1', 'tag2', 'tag3']),
+		price: parseInt(faker.commerce.price(), 10),
+	});
+}
+
+console.log(products);
